3a1e2fcae7e7314ca8cb7d8d6b5cc53e7e829ef0
DorkoSpron0 <tareassec9@gmail.com>
2022-05-08 16:53:28 -0500

    Creando archivos/carpetas necesarios

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express()
+
+// SETTINGS
+app.set('port', process.env.PORT || 8080)
+
+// MIDDLEWARES
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+// STARTING THE SERVER
+app.listen(app.get('port'), () => {
+    console.log('Server started on port: ',app.get('port'))
+})
