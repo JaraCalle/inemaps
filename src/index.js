577b50d9d1c3ba54c5c8c6b4d3491996f4fbcff7
Tazana2 <danisanmez@gmail.com>
2022-09-10 14:42:25 -0500

    Resulto problema con los archivos estaticos

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const session = require('express-session');
 const flash = require('express-flash');
 const passport = require('passport');
 const path = require('path');
+const fs = require('fs');
+const url = require('url');
 
 const initializePassport = require('./passportConfig');
 
@@ -30,6 +32,33 @@ app.use(passport.session());
 app.use(flash());
 
 app.use(require('./routes/index.routes'));
+
+// Deja usar archivos estaticos
+require(__dirname  + '/lib/helper.js');
+
+app.get('/*.*', function(req, res) {
+
+    var options = url.parse(req.url, true);
+
+    var mime = Helper.getMime(options);
+    
+    serveFile(res, options.pathname, mime);
+
+});
+
+function serveFile(res, pathName, mime) {
+    
+    mime = mime || 'text/html';
+    
+    fs.readFile(__dirname + '/' + pathName, function (err, data) {
+        if (err) {
+            res.writeHead(500, {"Content-Type": "text/plain"});
+            return res.end('Error loading ' + pathName + " with Error: " + err);
+        }
+        res.writeHead(200, {"Content-Type": mime});
+        res.end(data);
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
