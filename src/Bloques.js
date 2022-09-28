d9b8ddbbf84d5fb7b0539737aa69a585f8b0eb41
bethcc <nulbanyc@gmail.com>
2022-09-28 16:20:37 -0500

    Se crea entrada de Ids para los bloques y se corrije los scroll en firefox

--- src/Bloques.js
+++ src/Bloques.js
@@ -1,35 +1,42 @@
 module.exports = [
     {
+        id: 1,
         img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
         Titulo: 'Bloque 7A',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     },
     {
+        id: 2,
         img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
         Titulo: 'Bloque 7B',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     },
     {
+        id: 3,
         img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
         Titulo: 'Bloque 6A',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     },
     {
+        id: 4,
         img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
         Titulo: 'Bloque 6B',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     },
     {
+        id: 5,
         img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
         Titulo: 'Bloque 5A',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     },
     {
+        id: 6,
         img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
         Titulo: 'Bloque 5B',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     },
     {
+        id: 7,
         img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
         Titulo: 'Bloque 4',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
