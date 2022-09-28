38f610e29e4be9cec959027fc165f9eb456d802a
unknown <jimena.munozgomez@inemjose.edu.co>
2022-09-28 16:21:51 -0500

    Se agregan las imagenes de los bloques

--- src/Bloques.js
+++ src/Bloques.js
@@ -1,37 +1,52 @@
 module.exports = [
     {
-        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
-        Titulo: 'Bloque 7A',
+        img: 'https://i.postimg.cc/gkfB5nDL/bloque1.jpg',
+        Titulo: 'Bloque 1',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     },
     {
-        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
-        Titulo: 'Bloque 7B',
+        img: 'https://i.postimg.cc/Y0KYwGXj/Bloque2.jpg',
+        Titulo: 'Bloque 2',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     },
     {
-        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
-        Titulo: 'Bloque 6A',
+        img: 'https://i.postimg.cc/bJb372xF/Bloque3.jpg',
+        Titulo: 'Bloque 3',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     },
     {
-        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
-        Titulo: 'Bloque 6B',
-        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
-    },
-    {
-        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
+        img: 'https://i.postimg.cc/MpqHVk3c/Bloque5a.jpg',
         Titulo: 'Bloque 5A',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     },
     {
-        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
+        img: 'https://i.postimg.cc/MGX96LS4/bloque5b.jpg',
         Titulo: 'Bloque 5B',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     },
     {
-        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
-        Titulo: 'Bloque 4',
+        img: 'https://i.postimg.cc/FK970tRW/Bloque6a.jpg',
+        Titulo: 'Bloque 6A',
+        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+    },
+    {
+        img: 'https://i.postimg.cc/76SzN14X/Bloque6b.jpg',
+        Titulo: 'Bloque 6B',
+        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+    },
+    {
+        img: 'https://i.postimg.cc/9MxhKKcf/Bloque7a.jpg',
+        Titulo: 'Bloque 7A',
+        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+    },
+    {
+        img: 'https://i.postimg.cc/RCKFNVC1/Bloque7b.jpg',
+        Titulo: 'Bloque 7B',
+        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+    },
+    {
+        img: 'https://i.postimg.cc/853yFgG9/Bloque8a.jpg',
+        Titulo: 'Bloque 8A',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     }
 ]
