84d52d25f9499d271c7ddd0511d94ebe69056156
PcMediaTecnica <jaracalle2041@gmail.com>
2022-09-28 14:25:59 -0500

    Se automatizó la parte de los bloques

--- src/Bloques.js
+++ src/Bloques.js
@@ -1,10 +1,37 @@
 module.exports = [
     {
+        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
         Titulo: 'Bloque 7A',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     },
     {
+        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
         Titulo: 'Bloque 7B',
+        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+    },
+    {
+        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
+        Titulo: 'Bloque 6A',
+        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+    },
+    {
+        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
+        Titulo: 'Bloque 6B',
+        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+    },
+    {
+        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
+        Titulo: 'Bloque 5A',
+        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+    },
+    {
+        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
+        Titulo: 'Bloque 5B',
+        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+    },
+    {
+        img: 'https://i.postimg.cc/fyd3vyhh/P1470500.jpg',
+        Titulo: 'Bloque 4',
         Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
     }
 ]
