45de800bf96d29aad12475ab527b6d105cd8f723
PcMediaTecnica <jaracalle2041@gmail.com>
2022-10-04 14:30:52 -0500

    Texto en español de bloques e index

--- src/Bloques.js
+++ src/Bloques.js
@@ -3,60 +3,66 @@ module.exports = [
         id: 1,
         img: 'https://i.postimg.cc/gkfB5nDL/bloque1.jpg',
         Titulo: 'Bloque 1',
-        Descripcion: 'Science department block. Inside it are the laboratories and the science teachers rooms.'
+        Descripcion: 'Bloque del departamento de ciencias. En el primer piso se puede encontar un laboratorio además de una sala para profesores de ciencias naturales. En su segundo piso hay otro laboratorio con mas espacio y otra sala de profesores de quimica y física.'
     },
     {
         id: 2,
         img: 'https://i.postimg.cc/Y0KYwGXj/Bloque2.jpg',
         Titulo: 'Bloque 2',
-        Descripcion: 'Arts department block. On the first floor of this are the music rooms and on the second floor are the kitchens.'
+        Descripcion: 'Bloque del departamento de artes. En el primer piso se pueden encontrar aulas donde están las salas de música con algunos instrumentos para usar por los estudiantes de esta modalidad.En el segundo piso están las cocinas.'
     },
     {
         id: 3,
         img: 'https://i.postimg.cc/bJb372xF/Bloque3.jpg',
         Titulo: 'Bloque 3',
-        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+        Descripcion: 'Bloque del departamento de comercial. En el primer piso se puede encontrar 2 salas de informatica, una de estas se usa principalmente para practicar y enseñar mecanografía, también se encuentra una bodega para guardar materiales. En el segundo piso se puede encontrar la administración principal de este bloque ademas de salones para practicas contando con herramientas'
+    },
+    {
+        id: 11,
+        img: 'https://i.postimg.cc/MpqHVk3c/Bloque5a.jpg',
+        Titulo: 'Bloque 4',
+        Descripcion: 'Bloque del departamento de industrial. En el primer piso se puede encontrar aulas para carpintería y una area bastabte amplia para la soldadura y recorte de materiales. En el segundo piso se encuentra una aula para el diseño de modas además contando con una bodega que embarca los dos pisos para materiales.'
     },
     {
         id: 4,
         img: 'https://i.postimg.cc/MpqHVk3c/Bloque5a.jpg',
         Titulo: 'Bloque 5A',
-        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+        Descripcion: 'Bloque del departamento de sociales. En este bloque puedes encontrar la unidad perteneciente al grado undecimo, además de contar con los horarios de este grado y la programación semanal con los cuadros de honor.'
     },
     {
         id: 5,
         img: 'https://i.postimg.cc/MGX96LS4/bloque5b.jpg',
         Titulo: 'Bloque 5B',
-        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+        Descripcion: 'Bloque del departamento de matematicas. En este bloque puedes encontrar la unidad perteneciente al grado decimo, además de contar con los horarios de este grado. En este bloque puedes encontrar un aula que cuenta con cocina integrada para especialmente los profesores de estos dos bloques. '
     },
     {
         id: 6,
         img: 'https://i.postimg.cc/FK970tRW/Bloque6a.jpg',
         Titulo: 'Bloque 6A',
-        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+        Descripcion: 'Este bloque no cuenta con departamento, pero en este bloque se encuentra la cafetería de profesores, donde puedes conseguir un almuerzo a un precio no tan accesible, además de la unidad perteneciente al grado noveno'
     },
     {
         id: 7,
         img: 'https://i.postimg.cc/76SzN14X/Bloque6b.jpg',
         Titulo: 'Bloque 6B',
-        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+        Descripcion: 'Bloque de perteneciente al departamento de religión, desde el cual se la entrega de ayudas hacia los estudiantes. Este es a su vez el bloque de la unidad de grado octavo.'
     },
     {
         id: 8,
         img: 'https://i.postimg.cc/9MxhKKcf/Bloque7a.jpg',
         Titulo: 'Bloque 7A',
-        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+        Descripcion: 'Este bloque pertenece al departamente de ingles y a la unidad de quinto y sexto. Es el bloque ubicado al frente de el aula multiple en la cual se realizan las reuniones institucionales.'
     },
     {
         id: 9,
         img: 'https://i.postimg.cc/RCKFNVC1/Bloque7b.jpg',
         Titulo: 'Bloque 7B',
-        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+        Descripcion: 'Este es el bloque de español y de la unidad de septimo. Bloque ubicado al frente de la biblioteca.'
     },
     {
         id: 10,
         img: 'https://i.postimg.cc/853yFgG9/Bloque8a.jpg',
-        Titulo: 'Bloque 8A',
-        Descripcion: 'orem ipsum dolor sit amet consectetur adipisicing elit. Atque totam, omnis natus sed praesentium dolor. Lorem ipsum dolor sit'
+        Titulo: 'Bloque 8',
+        Descripcion: 'El bloque más bonito del colegio contiene las salas de programación, la biblioteca, la sala de proyecciones y todo el progreso técnologico del INEM. También tiene una tienda muy coqueta con un señor al que no debes pagarle con billetes mayores a diez mil.'
     }
 ]
