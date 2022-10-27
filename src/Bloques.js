70d6574d487e5fc85792e3cc38973c05c3ff5bb0
jimena <jimena.munozgomez@inemjose.edu.co>
2022-10-27 18:58:13 -0500

    Info de los bloques

--- src/Bloques.js
+++ src/Bloques.js
@@ -2,79 +2,85 @@ module.exports = [
     {
         id: 1,
         img: 'https://i.postimg.cc/gkfB5nDL/bloque1.jpg',
-        Titulo: 'Bloque 1',
-        Descripcion: 'Bloque del departamento de ciencias. En el primer piso se puede encontar un laboratorio además de una sala para profesores de ciencias naturales. En su segundo piso hay otro laboratorio con mas espacio y otra sala de profesores de quimica y física.'
+        Titulo: 'Bloque 1 - Ciencias',
+        Descripcion: 'Dentro de el se pueden encontrar tres laboratorios, una sala de computación y además de esto cuenta con tres salas de profesores para los propios del departamento de ciencias.'
     },
     {
         id: 2,
         img: 'https://i.postimg.cc/Y0KYwGXj/Bloque2.jpg',
-        Titulo: 'Bloque 2',
-        Descripcion: 'Bloque del departamento de artes. En el primer piso se pueden encontrar aulas donde están las salas de música con algunos instrumentos para usar por los estudiantes de esta modalidad.En el segundo piso están las cocinas.'
+        Titulo: 'Bloque 2 - Artes',
+        Descripcion: 'Bloque del departamento de artes y promoción social. En el primer piso se pueden encontrar salas de música con algunos instrumentos para usar por los estudiantes de esta modalidad. En el segundo piso se encuentra una cocina. En estos dos pisos hay 3 salas para los profesores.'
     },
     {
         id: 3,
         img: 'https://i.postimg.cc/bJb372xF/Bloque3.jpg',
-        Titulo: 'Bloque 3',
-        Descripcion: 'Bloque del departamento de comercial. En el primer piso se puede encontrar 2 salas de informatica, una de estas se usa principalmente para practicar y enseñar mecanografía, también se encuentra una bodega para guardar materiales. En el segundo piso se puede encontrar la administración principal de este bloque ademas de salones para practicas contando con herramientas'
+        Titulo: 'Bloque 3 - Comercial',
+        Descripcion: 'Dentro de este se pueden encontrar tres salas de informatica, también se encuentra una bodega para guardar materiales. Además cuenta con cuatro empresas didacticas para adminitración y dos salas de profesores.'
     },
     {
         id: 11,
         img: 'https://i.postimg.cc/MpqHVk3c/Bloque5a.jpg',
-        Titulo: 'Bloque 4',
-        Descripcion: 'Bloque del departamento de industrial. En el primer piso se puede encontrar aulas para carpintería y una area bastabte amplia para la soldadura y recorte de materiales. En el segundo piso se encuentra una aula para el diseño de modas además contando con una bodega que embarca los dos pisos para materiales.'
+        Titulo: 'Bloque 4 - Industriales',
+        Descripcion: 'En este se pueden encontrar salones tanto de diseño de modas como de dibujo tecnico. También cuenta con cinco salas de computacion y multiples talleres de construccion, energia fotovoltaica, electronica y automatización. Y una area bastabte amplia para la soldadura y recorte de materiales.'
     },
     {
         id: 4,
         img: 'https://i.postimg.cc/MpqHVk3c/Bloque5a.jpg',
-        Titulo: 'Bloque 5A',
-        Descripcion: 'Bloque del departamento de sociales. En este bloque puedes encontrar la unidad perteneciente al grado undecimo, además de contar con los horarios de este grado y la programación semanal con los cuadros de honor.'
+        Titulo: 'Bloque 5A - Sociales',
+        Descripcion: 'En este bloque puedes encontrar la unidad perteneciente al grado undecimo, la sicoorientacion, una sala de profesores y además de contar con los horarios de este grado y la programación semanal con los cuadros de honor.'
     },
     {
         id: 5,
         img: 'https://i.postimg.cc/MGX96LS4/bloque5b.jpg',
-        Titulo: 'Bloque 5B',
-        Descripcion: 'Bloque del departamento de matematicas. En este bloque puedes encontrar la unidad perteneciente al grado decimo, además de contar con los horarios de este grado. En este bloque puedes encontrar un aula que cuenta con cocina integrada para especialmente los profesores de estos dos bloques. '
+        Titulo: 'Bloque 5B - Matematicas',
+        Descripcion: 'En este bloque puedes encontrar la unidad perteneciente al grado decimo,la sicoorientacion, una sala de profesores además de contar con los horarios de este grado. En este bloque puedes encontrar tambien un aula taller para los estudiantes. '
     },
     {
         id: 6,
         img: 'https://i.postimg.cc/FK970tRW/Bloque6a.jpg',
         Titulo: 'Bloque 6A',
-        Descripcion: 'Este bloque no cuenta con departamento, pero en este bloque se encuentra la cafetería de profesores, donde puedes conseguir un almuerzo a un precio no tan accesible, además de la unidad perteneciente al grado noveno'
+        Descripcion: 'Este bloque no cuenta con departamento, pero en este bloque se encuentra la cafetería de profesores, donde puedes conseguir un almuerzo a un precio no tan accesible, además de la unidad perteneciente al grado noveno y la sicoorientacion.'
     },
     {
         id: 7,
         img: 'https://i.postimg.cc/76SzN14X/Bloque6b.jpg',
-        Titulo: 'Bloque 6B',
-        Descripcion: 'Bloque de perteneciente al departamento de religión, desde el cual se la entrega de ayudas hacia los estudiantes. Este es a su vez el bloque de la unidad de grado octavo.'
+        Titulo: 'Bloque 6B - Religion',
+        Descripcion: 'Este es el bloque de la unidad de grado octavo, cuenta con la sicoorientacion, y una unidad de atencion integral que es para brindar ayuda a los estudiantes.'
     },
     {
         id: 8,
         img: 'https://i.postimg.cc/9MxhKKcf/Bloque7a.jpg',
-        Titulo: 'Bloque 7A',
-        Descripcion: 'Este bloque pertenece al departamente de ingles y a la unidad de quinto y sexto. Es el bloque ubicado al frente de el aula multiple en la cual se realizan las reuniones institucionales.'
+        Titulo: 'Bloque 7A -Ingles',
+        Descripcion: 'Este bloque pertenece a la unidad de septimo y octavo. Es el bloque ubicado al frente de el aula multiple en la cual se realizan las reuniones institucionales.'
     },
     {
         id: 9,
         img: 'https://i.postimg.cc/RCKFNVC1/Bloque7b.jpg',
-        Titulo: 'Bloque 7B',
-        Descripcion: 'Este es el bloque de español y de la unidad de septimo. Bloque ubicado al frente de la biblioteca.'
+        Titulo: 'Bloque 7B - Español',
+        Descripcion: 'Este bloque  partenece a la unidad de quinto y sexto. Bloque ubicado al frente de la capilla.'
     },
     {
         id: 10,
         img: 'https://i.postimg.cc/853yFgG9/Bloque8a.jpg',
-        Titulo: 'Bloque 8A',
-        Descripcion: 'El bloque más bonito del colegio contiene las salas de programación, la biblioteca, la sala de proyecciones y todo el progreso técnologico del INEM. También tiene una tienda muy coqueta con un señor al que no debes pagarle con billetes mayores a diez mil.'
+        Titulo: 'Bloque 8 - Biblioteca',
+        Descripcion: 'El bloque más bonito del colegio contiene la sala de programación, la biblioteca, la sala de proyecciones y todo el progreso técnologico del INEM.'
+    },
+    {
+        id: 11,
+        img: 'https://i.postimg.cc/853yFgG9/Bloque8a.jpg',
+        Titulo: 'Bloque 8A - Informatica',
+        Descripcion: 'El segundo bloque más bonito del colegio contiene cinco salas de informatica, la oficina del jefe de informatica y cuenta con una zona de aimentacion y auditorios de artes '
     },
     {
         id: 12,
         img: 'https://i.postimg.cc/853yFgG9/Bloque8a.jpg',
-        Titulo: 'Bloque 8B',
-        Descripcion: 'Administracion'
+        Titulo: 'Bloque 8B - Administracion',
+        Descripcion: 'Este bloue cuenta con la enfermeria, la secretaria de bienestar, la pagaduria, la oficina de calidad, cinem, almacen,supervicion de servicios generales, fuera de esto en su segundo piso cuenta con la subdireccion administrativa, y la rectoria y vicerrectoria academica. '
     },
     {
         id: 13,
         img: 'https://i.postimg.cc/853yFgG9/Bloque8a.jpg',
-        Titulo: 'Bloque 9',
-        Descripcion: 'Deportes'
+        Titulo: 'Bloque 9 - Educacion Fisica',
+        Descripcion: 'Este bloque abarca toda la zona de deportes, contando con dos gimnasios, dos coliseos, canchas de voleibol, futbol y basquetbol, ademas de esto cuenta con aulas de taekwondo y boxeo y una casita de artes que cuenta con instrumentos musicales.'
     }
 ]
